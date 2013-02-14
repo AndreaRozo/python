9bb21658b413d78b6fc3f16d302b9a67330ed49e
Andrea Rozo <ca.rozo2200@uniandes.edu.co>
2013-02-13 21:39:54 -0500

    Tarea2 Python

---
+++ frecuencias.py
@@ -0,0 +1,69 @@
+﻿#Tarea 2 - Física Computacional
+#Andrea Rozo Méndez
+#201015972
+
+#Se revisan los argumentos del programa
+import sys
+if (len(sys.argv) ==2):
+	#Se lee el archivo de entrada
+	archivoEntrada = open('%s'% sys.argv[1], 'r')
+
+	#Se carga todo el texto del archivo de entrada
+	texto = archivoEntrada.read()
+
+	#Verificación de la validez de los caracteres
+
+	x=len(texto)
+	caracteresValidos = []
+	frecuencias = []
+	invalidos = [" ",".",",",";",":","\n","\r","_","-","/","?","!","{","}","[","]","(",")","'",'"',"<",">","+","#","$","%","&","=","°","|","¬","*","@","~","^","`","¨","´","¿","¡"]
+	
+	for i in range (x):
+		y = texto[i]
+		valido = 1
+		
+		if(y in invalidos):
+			valido = 0
+
+		if (valido):
+			agregar = True
+			for j in range (len(caracteresValidos)):
+				if(y == caracteresValidos[j]):
+					agregar = False
+			if (agregar == True):
+				caracteresValidos.append(y)
+
+	for i in range (len(caracteresValidos)):
+		z = caracteresValidos[i]
+		frecuencias.append(0)
+		for j in range (x):
+			y = texto[j]
+			if (z == y):
+				frecuencias[i] = frecuencias[i]+1
+
+
+	for i in range (len(frecuencias)):
+		for j in range (i,len(frecuencias)):
+			primero = frecuencias[i]
+			posicion = i
+			if(primero < frecuencias[j]):
+				primero = frecuencias[j]
+				posicion = j
+			temp1 = frecuencias[i]
+			temp2 = caracteresValidos[i]
+			frecuencias[i] = primero
+			caracteresValidos[i] = caracteresValidos[posicion]
+			frecuencias[posicion] = temp1
+			caracteresValidos[posicion] = temp2
+
+	archivoEntrada.close()
+	
+	#Generación del archivo de salida
+
+	archivoSalida = open('frecuencias_%s'%sys.argv[1]+'.txt','w')
+	for i in range (len(caracteresValidos)):
+		archivoSalida.write('%s       %d\n' % (caracteresValidos[i], frecuencias[i]))
+
+	archivoSalida.close()
+else:
+	print "Este programa necesita un parametro"
